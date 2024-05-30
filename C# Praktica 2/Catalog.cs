090acfaf9d5285ecad2ce5328ee718523db65753
Alanqwd <nurmatovnurbek555@gmai.com>
2024-05-30 20:41:30 +0300

    Добавьте файлы проекта.

---
+++ C# Praktica 2/Catalog.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+
+namespace C__Praktica_2
+{
+    public class Catalog
+    {
+        public List<Product> products = new List<Product>();
+        public void AddProduct(Product product) {  products.Add(product); }
+        public void RemoveProduct(int id) 
+        {
+            products.RemoveAll(p => p.Id == id);
+        }
+        public void UpdateProduct(Product product)
+        {
+            var existing = products.FirstOrDefault(p => p.Id == product.Id);
+            if (existing != null) 
+            {
+                existing.Name = product.Name;
+                existing.Price = product.Price;
+                existing.Category = product.Category;
+            }
+        }
+        public void SaveToJson(string path)
+        {
+            string json = JsonConvert.SerializeObject(products);
+            File.WriteAllText(path, json);
+        }
+
+        public void LoadFromJson(string path)
+        {
+            string json = File.ReadAllText(path);
+            products = JsonConvert.DeserializeObject<List<Product>>(json);
+        }
+        public void SaveToXml(string path) 
+        {
+            XElement xml = new XElement("Products",
+                from product in products select new XElement("Product", 
+                new XAttribute("Id",product.Id),
+                new XAttribute("Name",product.Name),
+                new XAttribute("Price",product.Price),
+                new XAttribute("Category",product.Category)));
+            xml.Save(path);
+        }
+        public void LoadFromXml(string path) 
+        {
+            var culture = System.Globalization.CultureInfo.InvariantCulture;
+            XDocument xml = XDocument.Load(path);
+            products = xml.Descendants("Product")
+                .Select(p=> new Product
+                {
+                    Id = int.Parse(p.Attribute("Id").Value),
+                    Name = p.Attribute("Name").Value,
+                    Price = double.Parse(p.Attribute("Price").Value,culture),
+                    Category = p.Attribute("Category").Value
+                }).ToList();
+
+
+        }
+
+        public IEnumerable<Product> FiltrProductsByCategory(string category)
+        {
+            return products.Where(p => p.Category.ToLower() == category.ToLower());
+        }
+
+        public IEnumerable<Product> FiltrProductsByPrice(double min, double max)
+        {
+            return products.Where(p => p.Price>=min && p.Price<=max);
+        }
+
+        public IEnumerable<Product> SortedByPrice()
+        {
+            return products.OrderBy(p=> p.Price);
+        }
+        public IEnumerable<Product> SortedByName()
+        {
+            return products.OrderBy(p => p.Name);
+        }
+
+    }
+
+}
---
+++ C# Praktica 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.SqlTypes;
+
+namespace C__Praktica_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Catalog catalog = new Catalog();
+            catalog.AddProduct(new Product { Id = 1, Name = "Moloko", Price = 78.50, Category = "MilkProduct" });
+            catalog.AddProduct(new Product { Id = 2, Name = "Bread" , Price = 58.20, Category = "Bacaley" });
+            catalog.AddProduct(new Product { Id = 3, Name = "Fish", Price = 45.20, Category = "FishProduct" });
+
+            catalog.SaveToJson("products.json");
+            catalog.LoadFromJson("products.json");
+            foreach(var i in catalog.products) 
+            { 
+                Console.WriteLine($"ID: {i.Id}, Name:{i.Name},Price: {i.Price},Category: {i.Category}");
+            }
+            Console.WriteLine();
+
+            catalog.SaveToXml("products.xml");
+            catalog.LoadFromXml("products.xml");
+            foreach (var i in catalog.products)
+            {
+                Console.WriteLine($"ID: {i.Id}, Name:{i.Name},Price: {i.Price},Category: {i.Category}");
+            }
+            Console.WriteLine();
+
+            IEnumerable<Product> filteredProduct = catalog.FiltrProductsByCategory("MilkProduct");
+            foreach (var i in filteredProduct)
+            {
+                Console.WriteLine($"ID: {i.Id}, Name:{i.Name},Price: {i.Price},Category: {i.Category}");
+            }
+            Console.WriteLine();
+
+            IEnumerable<Product> filteredPrice = catalog.FiltrProductsByPrice(20.50,60.50);
+            foreach (var i in filteredPrice)
+            {
+                Console.WriteLine($"ID: {i.Id}, Name:{i.Name},Price: {i.Price},Category: {i.Category}");
+            }
+        }
+
+        
+    }
+}
